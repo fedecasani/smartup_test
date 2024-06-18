05c7d8aeb25e5c2ae82d8f99f5bcd1c051940c6d
Federico Casani <fedecasani@gmail.com>
2024-06-18 15:19:30 -0300

    configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,21 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 
-void main() {
+Future main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+
+  if(kIsWeb) {
+    await Firebase.initializeApp(
+      options: FirebaseOptions(
+        apiKey: "AIzaSyBQ38Cmzu60XQXhzJvuk3BtCxnL8gtR4Ag", 
+        appId: "1:488939344201:web:97a5072bc1eb4ceb688a98", 
+        messagingSenderId: "488939344201", 
+        projectId: "smartup-test-d823e")
+    );
+  }
+
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
